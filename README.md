9a757093a20b68baeed6030fbfdbf43d5f47e159
yawkat <me@yawk.at>
2017-04-24 18:47:17 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+hawotemp-graphite
+-----------------
+
+Simple daemon that collects data from [hawotemp](https://hawo.net/service/temperatur.html) and throws it into graphite
---
+++ daemon.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import re
+import time
+import traceback
+
+import graphitesend
+
+graphitesend.init(graphite_server="192.168.1.3", prefix="hawotemp", system_name="")
+
+
+def telnet_get(host: str, port: int) -> str:
+    import socket
+    with socket.create_connection((host, port)) as s:
+        return s.makefile().read()
+
+
+while True:
+    collected_data = {}
+    # noinspection PyBroadException
+    try:
+        for sensor in ("east-temp", "west-temp"):
+            lines = telnet_get(sensor + ".hawo.stw.uni-erlangen.de", 7338).splitlines()
+            temp = float(re.match(r"(.+)C", lines[0]).group(1))
+            humidity = float(re.match(r"(.+)% Humidity", lines[1]).group(1)) / 100.
+            collected_data[sensor + ".temperature"] = temp
+            collected_data[sensor + ".humidity"] = humidity
+    except:
+        traceback.print_exc()
+    if len(collected_data) > 0:
+        graphitesend.send_dict(collected_data)
+    time.sleep(30)
